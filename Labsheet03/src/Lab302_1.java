9f34acd9b1fa9dcadb49a9ef31b5833492c9eb32
ChaiyaphonCS <blackgon15@gmail.com>
2024-12-11 10:25:10 +0700

    Labsheet03Example

---
+++ Labsheet03/src/Lab302_1.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+public class Lab302_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean chkEmail = false;
+		String inputEmail;
+		while (true) {
+			inputEmail = JOptionPane.showInputDialog("Input your e-mail:").toLowerCase();
+			while (inputEmail.startsWith("@")||inputEmail.contains(" ")) {
+				inputEmail = JOptionPane.showInputDialog("Input your e-mail,again:");
+			}
+			chkEmail = inputEmail.endsWith("@gmail.com")||inputEmail.endsWith("hotmail.com");
+			if(chkEmail) {
+				JOptionPane.showMessageDialog(null,"Your e-mail is "+inputEmail);
+				break;
+			}else {
+				JOptionPane.showMessageDialog(null,"Your e-mail is not hotmail or gmail dot com");
+				continue;
+			}
+		}
+
+
+	}
+
+}
