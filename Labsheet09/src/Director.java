25a2226cab04e47715d62c41a46b5217cc08abd1
ChaiyaphonCS <blackgon15@gmail.com>
2025-02-05 11:24:12 +0700

    Labsheet09_Practice

---
+++ Labsheet09/src/Director.java
@@ -0,0 +1,37 @@
+
+public class Director {
+private String name;
+private String email;
+private char gender;
+
+Director(String name,String email,char gender){
+	this.name = name;
+	this.email = email;
+	this.gender = gender;
+}
+
+Director(String name,String email){
+	this(name,email,' ');
+	
+}
+public String getName() {
+	return this.name;
+}
+public String getEmail() {
+	return this.email;
+}
+public String getGenderName() {
+	if (this.gender == 'M') {
+		return "Male";
+	}
+	else if (this.gender == 'F') {
+		return "Female";
+	}
+	else {
+		return null;
+	}
+}
+public String toString() {
+	return getName()+"("+getEmail()+";"+getGenderName()+")";	
+}
+}
---
+++ Labsheet09/src/Theater.java
@@ -0,0 +1,8 @@
+
+public class Theater extends Movie{
+private int theaterNo;
+
+Theater (String id,String name,Director director,int theaterNo){
+	 
+}
+}
