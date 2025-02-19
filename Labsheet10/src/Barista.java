e49eaaa2b12371ae23c1d7347a24d6b1998822df
ChaiyaphonCS <blackgon15@gmail.com>
2025-02-19 11:12:21 +0700

    Labsheet10Practice

---
+++ Labsheet10/src/Barista.java
@@ -0,0 +1,22 @@
+
+public class Barista {
+	private String name;
+	private char gender;
+	
+	Barista(String name , char gender){
+		this.name = name;
+		this.gender = gender;
+	}
+	Barista(){
+		this(null,' ');
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getGenderName() {
+		if (this.gender == 'M') {
+			return "Male";
+		}
+	}
+
+}
---
+++ Labsheet10/src/Drink.java
@@ -0,0 +1,30 @@
+
+public class Drink {
+	private int type;
+	private char size;
+	
+	Drink(int type,char size){
+		this.type = type;
+		this.size = size;
+	}
+	Drink(){
+		this(0,' ');
+	}
+	public String getTypeName() {
+		
+	}
+	public int getTypePrice() {
+		
+	}
+	public String getSizeName() {
+		
+	}
+	public int getSizePrice() {
+		
+	}
+	public int getTotalPrice() {
+		
+	}
+		
+ 
+}
